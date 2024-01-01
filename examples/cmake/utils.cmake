f355c928bb8d4b25d1dc52c504f17d3d48572e16
Konovalov <fizteh.science@yandex.ru>
2024-01-02 02:00:48 +0300

    Change utils.cmake for building dll with gcc

--- examples/cmake/utils.cmake
+++ examples/cmake/utils.cmake
@@ -39,10 +39,12 @@ function(win_copy_deps_to_target_dir target)
                         "$<TARGET_FILE:${dep}>" "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
                     COMMAND_EXPAND_LISTS)
             else()
-                add_custom_command(TARGET ${target} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -P "${libmath_SOURCE_DIR}/cmake/silent_copy.cmake"
-                        "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
-                    COMMAND_EXPAND_LISTS)
+                if (MSVC)
+                    add_custom_command(TARGET ${target} POST_BUILD
+                        COMMAND ${CMAKE_COMMAND} -P "${libmath_SOURCE_DIR}/cmake/silent_copy.cmake"
+                            "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
+                        COMMAND_EXPAND_LISTS)
+                endif()
             endif()
         endif()
     endforeach()

12c5a30000e868e99b168a5124f0317b35092353
Konovalov <fizteh.science@yandex.ru>
2024-01-16 23:45:30 +0300

    Add test and example for polygone interpolator

--- libmath/interpolator/bilinear_interpolator.h
+++ libmath/interpolator/bilinear_interpolator.h
@@ -1,19 +1,53 @@
 #pragma once
 #include <libmath/interpolator/interpolator.h>
+#include <algorithm>
+#include <cmath>
+#include <omp.h>
 
 namespace math
 {
-
 	template <typename T>
 	class BiLinearInterpolator : public Interpolator<T>
 	{
 	private:
+        /// @brief Matrix of data coefficients (from independent variables points)
+        Matrix<T> A_;
+
+        /// @brief Vector of data free terms (dependent variables points)
+        Matrix<T> b_;
+
+        /// @brief Vector of interpolation coefficients
+        Matrix<T> c_;
 	public:
-		BiLinearInterpolator()
-		{
-			Interpolator<T>::method_ = "BiLinear";
-		};
+        BiLinearInterpolator() : Interpolator<T>("BiLinear") {};
+
+        BiLinearInterpolator(const math::Matrix<T>& x, const math::Matrix<T>& y) : Interpolator<T>("BiLinear", x, y)
+        {
+			A_ = Matrix<T>(std::pow(2,Interpolator<T>::dim_));
+            b_ = static_cast<T>(-1.0) * y;
+            c_ = Matrix<T>(std::pow(2,Interpolator<T>::dim_), 1);
+        };
 
 		virtual ~BiLinearInterpolator(){};
+
+        virtual void build() override
+        {
+			for (int pos = 0; pos < A_.numel(); ++pos)
+			{
+				size_t row { 0 };
+				size_t col { 0 };
+
+				// A_ has row representation by default
+				row = (size_t)std::floor(pos / A_.cols());
+				col = pos - row * A_.cols();
+
+				
+			}
+		}
+		
+		virtual T interpolate(const Matrix<T>& x) override
+		{
+			return 1.0;
+		}
 	};
 }
--- libmath/interpolator/interpolator.test.cpp
+++ libmath/interpolator/interpolator.test.cpp
@@ -5,35 +5,117 @@
 
 TEST(Interpolator, Polygone)
 {
-	math::Matrix<double> x =
+	// check 1d
+
+	math::Matrix<double> x1 =
+	{
+		{1.},
+		{2.}
+	};
+
+	math::Matrix<double> y1 =
+	{
+		{1.},
+		{2.}
+	};
+
+	math::PolygoneInterpolator<double> polygone1(x1, y1);
+	polygone1.build();
+
+	double y_1d1 = polygone1.interpolate({{1.}});
+	EXPECT_EQ(math::isEqual(y_1d1, 1.0), true);
+
+	double y_1d2 = polygone1.interpolate({{1.5}});
+	EXPECT_EQ(math::isEqual(y_1d2, 1.5), true);
+
+	double y_1d3 = polygone1.interpolate({{3.}});
+	EXPECT_EQ(math::isEqual(y_1d3, 3.), true);
+	
+	// check 2d
+
+	math::Matrix<double> x2 =
 	{
 		{1.,1.},
 		{2.,1.},
 		{1.,2.}
 	};
 
-	math::Matrix<double> y =
+	math::Matrix<double> y2 =
 	{
 		{1.},
 		{2.},
 		{2.}
 	};
 
-	math::PolygoneInterpolator<double> polygone(x, y);
-	polygone.build();
+	math::PolygoneInterpolator<double> polygone2(x2, y2);
+	polygone2.build();
 
-	math::Matrix<double> new_x = 
+	double y_2d1 = polygone2.interpolate({{2., 1.}});
+	EXPECT_EQ(math::isEqual(y_2d1, 2.0), true);
+
+	double y_2d2 = polygone2.interpolate({{1., 1.5}});
+	EXPECT_EQ(math::isEqual(y_2d2, 1.5), true);
+
+	double y_2d3 = polygone2.interpolate({{2., 2.}});
+	EXPECT_EQ(math::isEqual(y_2d3, 3.), true);
+	
+	// check 3d
+
+	math::Matrix<double> x3 =
 	{
-		{2., 1.}
+		{1.,1.,1.},
+		{2.,1.,2.},
+		{1.,2.,2.},
+		{3.,1.,2.}
 	};
-	double y_int = polygone.interpolate(new_x);
 
-	std::cout << y_int << std::endl;
+	math::Matrix<double> y3 =
+	{
+		{1.},
+		{2.},
+		{2.},
+		{3.}
+	};
+
+	math::PolygoneInterpolator<double> polygone3(x3, y3);
+	polygone3.build();
+
+	double y_3d1 = polygone3.interpolate({{2., 1., 2.}});
+	EXPECT_EQ(math::isEqual(y_3d1, 2.0), true);
+
+	double y_3d2 = polygone3.interpolate({{1., 1.5, 1.5}});
+	EXPECT_EQ(math::isEqual(y_3d2, 1.5), true);
+
+	double y_3d3 = polygone3.interpolate({{3., 3., 3.}});
+	EXPECT_EQ(math::isEqual(y_3d3, 5.), true);
 }
 
-TEST(Interpolator, BiLinear)
-{
-	// math::BiLinearInterpolator<double> bilinear;
+// TEST(Interpolator, BiLinear)
+// {
+// 	math::Matrix<double> x =
+// 	{
+// 		{1.,1.},
+// 		{2.,1.},
+// 		{1.,2.},
+// 		{2.,2.}
+// 	};
 
-	std::cout << "hi" << std::endl;
-}
+// 	math::Matrix<double> y =
+// 	{
+// 		{1.},
+// 		{1.},
+// 		{1.},
+// 		{1.}
+// 	};
+
+// 	math::BiLinearInterpolator<double> bilinear(x, y);
+// 	bilinear.build();
+
+// 	math::Matrix<double> new_x = 
+// 	{
+// 		{2., 1.}
+// 	};
+// 	double y_int = bilinear.interpolate(new_x);
+
+// 	std::cout << y_int << std::endl;
+// }
